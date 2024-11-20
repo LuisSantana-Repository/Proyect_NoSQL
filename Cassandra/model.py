d4eddfcf818acccc6e18b6ce8bb41968d9527772
LuisSantana-Repository <117953762+LuisSantana-Repository@users.noreply.github.com>
2024-11-19 23:05:14 -0600

    started inserts

--- Cassandra/model.py
+++ Cassandra/model.py
@@ -81,14 +81,34 @@ CREATE_COMMENTS_COUNT = """
     );
 """
 
+def insert_Post(userUUID,session, post,tags,categoty,lenguage):
+    pbu_stmt = session.prepare("INSERT INTO Posts_by_user (user_id, post_id, content, timestamp, tags, category, language) VALUES(?,?,?,?,?,?,?)")
+    pbt_stmt = session.prepare("INSERT INTO Posts_by_topic (user_id, post_id, content, timestamp, tags, category, language) VALUES(?,?,?,?,?,?,?)")
+    pbp_stmt = session.prepare("INSERT INTO Posts_by_parent (user_id, post_id, content, timestamp, tags, category, language) VALUES(?,?,?,?,?,?,?)")
+    plc_stmt = session.prepare("INSERT INTO Post_likes_count(post_id,likes_count) VALUES(?,?)")
+    pcc_stmt = session.prepare("INSERT INTO Post_comments_count(post_id,comments_count) VALUES(?,?)")
+    Activity_stmt = session.prepare("INSERT INTO User_activity (user_id, action_type, target_id, timestamp) VALUES(?,?,?,?)")
+    Topics_stmt = session.prepare("INSERT INTO Post_topics (post_id, topic) VALUES(?, ?)")
+    Hastags_stmt = session.prepare("INSERT INTO Post_hashtags (post_id, hashtag) VALUES(?, ?)")
+
+    
+def insertComment(userUUID,session, post,tags,categoty,lenguage, parentUUID):
+    pbu_stmt = session.prepare("INSERT INTO Posts_by_user (user_id, post_id, content, timestamp, tags, category, language) VALUES(?,?,?,?,?,?,?)")
+    pbt_stmt = session.prepare("INSERT INTO Posts_by_topic (user_id, post_id, content, timestamp, tags, category, language) VALUES(?,?,?,?,?,?,?)")
+    pbp_stmt = session.prepare("INSERT INTO Posts_by_parent (user_id, post_id, content, timestamp, tags, category, language) VALUES(?,?,?,?,?,?,?)")
+    plc_stmt = session.prepare("INSERT INTO Post_likes_count(post_id,likes_count) VALUES(?,?)")
+    pcc_stmt = session.prepare("INSERT INTO Post_comments_count(post_id,comments_count) VALUES(?,?)")
+    Activity_stmt = session.prepare("INSERT INTO User_activity (user_id, action_type, target_id, timestamp) VALUES(?,?,?,?)")
+    Topics_stmt = session.prepare("INSERT INTO Post_topics (post_id, topic) VALUES(?, ?)")
+    Hastags_stmt = session.prepare("INSERT INTO Post_hashtags (post_id, hashtag) VALUES(?, ?)")
 
 
+def insert_logIn(session,user,ip):
+    lbu_stmt = session.prepare("INSERT INTO Login_by_user (user_id, timestamp, ip_address) VALUES(?,?,?)")
+    lbd_stmt = session.prepare("INSERT INTO Login_by_device (device_id, timestamp, user_id) VALUES(?,?,?)")
+    Activity_stmt = session.prepare("INSERT INTO User_activity (user_id, action_type, target_id, timestamp) VALUES(?,?,?,?)")
 
-
-
-
-
-
-
-
-
+def insert_Likes(session,posts,):
+    Activity_stmt = session.prepare("INSERT INTO User_activity (user_id, action_type, target_id, timestamp) VALUES(?,?,?,?)")
+    likebu_stmt = session.prepare("INSERT INTO Likes_by_user (user_id, post_id, timestamp) VALUES(?,?,?)")
+    plc_stmt = session.prepare("UPDATE Post_likes_count SET likes_count = likes_count + 1 WHERE post_id = ?")
